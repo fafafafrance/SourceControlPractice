99c0f2d9f853b9b6d224fa718f3cde76329144ae
France A <133005536+fafafafrance@users.noreply.github.com>
2026-07-24 00:48:05 -0700

    Completed ThreeDPrinter class practice

---
+++ ThreeDPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SourceControlPractice
+{
+    public class ThreeDPrinter
+    {
+        // Auto Implented properties
+        public string ProductName { get; set; }
+        public string BuildVolume { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime DateRelease { get; set; }
+        public List<string> SupportFilament { get; set; }
+
+        // Constructor to initialize the collection
+        public ThreeDPrinter() 
+        {
+            SupportFilament = new List<string>();
+        }
+    }
+}
